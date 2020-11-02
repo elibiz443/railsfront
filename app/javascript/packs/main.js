fe70b4ce4795ee6e161926ba0b1f6aae63b4683e
elibiz443 <elibiz443@gmail.com>
2020-11-02 19:20:21 +0300

    Modify JS

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -257,7 +257,7 @@ $.scrollUp({
   animation: 'fade', // Fade, slide, none
   animationInSpeed: 200, // Animation in speed (ms)
   animationOutSpeed: 200, // Animation out speed (ms)
-  scrollText: '^', // Text for element
+  scrollText: 'UP', // Text for element
   activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
 });
 
